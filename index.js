dbe847587fad7d2becefdb5421e6752e33b59798
shiory602 <suzushioufo@gmail.com>
2021-08-24 20:30:24 -0700

    basicAuth

--- index.js
+++ index.js
@@ -3,77 +3,86 @@ const basicAuth = require('express-basic-auth')
 const app = express()
 
 
-class SimpleCrudDB {
-  // store your records here
-  inMemoryDatabase = []
-  // import this and begin to use it
-  fs = require('fs')
+// class SimpleCrudDB {
+//   // store your records here
+//   inMemoryDatabase = []
+//   // import this and begin to use it
+//   fs = require('fs')
 
-  constructor(aFileName) {
-    this.fileName = aFileName;
-    // this is the filename you will save to add code below to create that file, or
-    // open it when this class is instantiated, ie: your program start, it loads the database from the file, into the this.inMemoryDatabase
-  }
+  
 
-  create(username, password) {
-    // add code to store a record in your database
-    // return true if success, false if not
-  }
+//   constructor(aFileName) {
+//     this.fileName = aFileName;
+//     // this is the filename you will save to add code below to create that file, or
+//     // open it when this class is instantiated, ie: your program start, it loads the database from the file, into the this.inMemoryDatabase
+//   }
 
-
-  Read(username) {
-    // add code to read record from database
-      // return undefined if no record exists, otherwise, return the record
-  }
+//   create(username, password) {
+//     // add code to store a record in your database
+//     // return true if success, false if not
+//   }
 
 
+//   Read(username) {
+//     // add code to read record from database
+//       // return undefined if no record exists, otherwise, return the record
+//   }
 
 
-  Update(username, newRecordData) {
-    // add code to update a record in the database
 
 
-    // return true if success, false if not
+//   Update(username, newRecordData) {
+//     // add code to update a record in the database
 
 
-  };
+//     // return true if success, false if not
 
 
-  Delete(username)
-  {
-  // add code to delete a user in the database
+//   };
 
 
-  // return true if success, false if not
+//   Delete(username)
+//   {
+//   // add code to delete a user in the database
 
 
-  };
+//   // return true if success, false if not
 
 
-  flushDB() {
-      // flush the inmemoryDatabase to disk (ie: save the database to disk)
-  }
+//   };
 
 
-  reloadDB() {
-      // relead the database from disk
-  }
+//   flushDB() {
+//       // flush the inmemoryDatabase to disk (ie: save the database to disk)
+//       fs.writeFile('your db path', JSON.stringify(this.inMemoryDatabase) , (err) => {
+//         if (err)
+//           console.log(err);
+//         else {
+//           console.log("File written successfully")
+//       }})
+//   }
 
 
-}
-let db = new SimpleCrudDB('./db.json')
+//   reloadDB() {
+//       // relead the database from disk
+//   }
+
+
+// }
+// let db = new SimpleCrudDB('./db.json')
 
 
 app.use(basicAuth({
-  authorizer: (username, password) => {
-    const userMatches = basicAuth.safeCompare(username, 'admin')
-    const passwordMatches = basicAuth.safeCompare(password, 'supersecret')
-    return userMatches & passwordMatches
-  }
+  users: { 'admin': 'supersecret' },
+  challenge: true
 }))
 
 app.get('/', (req, res) => {
   res.send('authorized')
+})
+
+app.get('/version', (req,res,net) => {
+  res.send('1. 0. 0')
 })
 
 app.listen(3000, () => console.log('server started'))
